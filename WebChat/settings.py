17a252071775dd0f45ecc99f92b9c55b75957415
Artem <artemis.gorb.14@gmail.com>
2021-01-20 17:01:23 +0300

    deploying to heroku

--- WebChat/settings.py
+++ WebChat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,5 @@ STATICFILES_DIRS = [STATIC_DIR]
 LOGIN_URl = '/auth/login/'
 
 LOW_CACHE = True
+
+django_heroku.settings(locals())
